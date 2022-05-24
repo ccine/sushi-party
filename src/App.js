c1a8db797bbdd963939b126d703e13378247cc84
ccine <56122695+ccine@users.noreply.github.com>
2022-05-24 23:10:26 +0200

    Add form and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.png';
-import './App.css';
-import Image from 'react-bootstrap/Image';
+import logo from "./logo.png";
+import { Image, Form, Button } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Image src={logo} width="60%"  className="App-logo" alt="logo"/>
+      <Image src={logo} width="60%" className="App-logo" alt="logo" />
+      <h1>Sushi Party</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Join Party
+        </Button>
+      </Form>
+      <Button variant="danger" type="submit">Create Party</Button>
     </div>
   );
 }
