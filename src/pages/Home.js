f15d5ef348e2a1981dde9dd990d79d54aa92bff4
Mastro-Zem <106116732+Mastro-Zem@users.noreply.github.com>
2022-05-30 14:59:08 +0200

    Added custom.scss
    Added custom.scss which overrides some bootstrap variables.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,34 +2,46 @@ import React from "react";
 import logo from "../antisgamosgobbo.png";
 import { Image, Form, Button } from "react-bootstrap";
 import "../App.css";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
+import "../custom.scss";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="Develop">
-    <Image src={logo} width="15%" className="App-logo" alt="logo" onClick={() => navigate('/')}/>
+    <div className="App">
+      <Image
+        src={logo}
+        width="15%"
+        className="App-logo"
+        alt="logo"
+        onClick={() => navigate("/")}
+      />
       <h1>Google Party</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Join Party
+      <div className="form-signin">
+        <Form>
+          <Form.Group className="mb-1" controlId="formBasicEmail">
+            <Form.Control
+              size="lg"
+              className="mb-1"
+              type="email"
+              placeholder="Nickname"
+            />
+            <Form.Control size="lg" type="password" placeholder="Party code" />
+          </Form.Group>
+          <Button
+            className="mb-1 w-100"
+            variant="primary"
+            size="lg"
+            type="submit"
+          >
+            Join Party
+          </Button>
+        </Form>
+        <Button className="w-100" variant="secondary" size="lg" type="submit" onClick={() => navigate('/Party')}>
+          Create Party
         </Button>
-      </Form>
-      <Button variant="danger" type="submit">Create Party</Button>
+      </div>
     </div>
   );
 }
